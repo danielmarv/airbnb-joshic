b81afc2902d2024331b9cd399121d0ef54b09799
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-04-29 15:48:17 +0300

    Change

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -101,7 +101,7 @@ const LoginModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => {}}
+                onClick={() => signIn('github')}
             />
             <div className="
                 text-neutral-500
