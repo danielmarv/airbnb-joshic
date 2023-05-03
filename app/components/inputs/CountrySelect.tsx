6a5c53edd697c8c13424399502d665eab9af7295
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-03 23:48:05 +0300

    Update CountrySelect.tsx

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+export type CountrySelectValue = {
+    flag: string;
+    label: string;
+    latling: number[];
+    region: string;
+    value: string;
+}
+
+interface Country
+
 const CountrySelect = () => {
     return (
         <div></div>
