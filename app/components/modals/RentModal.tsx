d7b621779b23e9700e9b6f0bc1eb17d08adb7b4a
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-04 12:12:55 +0300

    Commit

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,6 +48,7 @@ const RentModal = () => {
     });
 
     const category = watch('category');
+    const location = watch('location');
 
     const setCustomValue = (id: string, value: any) => {
         setValue (id, value, {
@@ -117,8 +119,10 @@ const RentModal = () => {
                     subtitle="Help guests find you!"
                 />
                 <CountrySelect 
-                     
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
                 />
+                <Map />
             </div>
         )
     }
